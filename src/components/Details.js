581b72346828441ef97ef4f7f86f3d67876dea8e
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-09 19:37:58 -0400

    Products and Detail Component finished

---
+++ src/components/Details.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
+export default class extends Component {
+  render() {
+    return (
+      <ProductConsumer>
+        {value => {
+          const {
+            id,
+            company,
+            img,
+            info,
+            price,
+            title,
+            inCart
+          } = value.detailProduct;
+          return (
+            <div className="container py-5">
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/* End Title */}
+              {/* Product Info */}
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                {/* Product Text */}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>model: {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    made by : <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      price : <span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <h4>
+                    <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                      information about product:
+                    </p>
+                    <p className="text-muted lead">{info}</p>
+                    {/* Buttons */}
+                    <div>
+                      <Link to="/">
+                        <ButtonContainer>back to products</ButtonContainer>
+                      </Link>
+                      <ButtonContainer
+                        cart
+                        disabled={inCart ? true : false}
+                        onClick={() => {
+                          value.addToCart(id);
+                        }}
+                      >
+                        {inCart ? "inCart" : "add to cart"}
+                      </ButtonContainer>
+                    </div>
+                  </h4>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
+    );
+  }
+}
---
+++ src/context.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { storeProduct, detailProduct, storeProducts } from "./data";
+
+const ProductContext = React.createContext();
+//Provider
+//Consumer
+
+class ProductProvider extends Component {
+  state = {
+    products: [],
+    detailProduct: detailProduct
+  };
+  componentDidMount() {
+    this.setProducts();
+  }
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  };
+
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+  addToCart = id => {
+    console.log(`hello from add to cart.id is ${id}`);
+  };
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
