c6a21be91c2919c8eeaa22964d408ecdf7480237
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 13:08:30 -0400

    Finished removeItem method

--- src/context.js
+++ src/context.js
@@ -76,7 +76,25 @@ class ProductProvider extends Component {
     console.log("decreased");
   };
   removeItem = id => {
-    console.log("item removed");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
   clearCart = () => {
     this.setState(
