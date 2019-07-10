b067f410c5cdf02d2cf9bb39fcf8a0847a80df1c
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 00:30:13 -0400

    Added function to open and close cart modal

--- src/context.js
+++ src/context.js
@@ -9,7 +9,9 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: []
+    cart: [],
+    modalOpen: true,
+    modalProduct: detailProduct
   };
   componentDidMount() {
     this.setProducts();
@@ -53,13 +55,26 @@ class ProductProvider extends Component {
       }
     );
   };
+  openModal = id => {
+    const product = this.getItem(id);
+    this.setState(()=>{
+      return {modalProduct:product,modalOpen:true}
+    })
+  }
+  closeModal = () => {
+    this.setState{() => {
+      return {modalOpen:false}
+    }}
+  }
   render() {
     return (
       <ProductContext.Provider
         value={{
           ...this.state,
           handleDetail: this.handleDetail,
-          addToCart: this.addToCart
+          addToCart: this.addToCart,
+          openModal:this.openModal,
+          closeModal:this.closeModal
         }}
       >
         {this.props.children}
