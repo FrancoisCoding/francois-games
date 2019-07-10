9511e30f707946c10836d4cf08a9db80bd3df201
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 11:19:41 -0400

    Created CartList and CartItem components

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import Title from "../Title";
 import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
 import { ProductConsumer } from "../../context";
+import CartList from "./CartList";
 export default class Cart extends Component {
   render() {
     return (
@@ -15,6 +16,7 @@ export default class Cart extends Component {
                 <React.Fragment>
                   <Title name="your" title="cart" />
                   <CartColumns />
+                  <CartList value={value} />
                 </React.Fragment>
               );
             } else {
---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CartItem from "./CartItem";
+export default () => {
+  return (
+    <div>
+      hello from cart list
+      <CartItem />
+    </div>
+  );
+};
