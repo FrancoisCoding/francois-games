919978e94106c23167e5aa3a6a6714b063c136a9
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 11:09:00 -0400

    Created cart folder and finished CartColumns component

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-title" />
+      </div>
+    </div>
+  );
+};
--- src/context.js
+++ src/context.js
@@ -11,7 +11,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
   componentDidMount() {
     this.setProducts();
@@ -66,6 +69,18 @@ class ProductProvider extends Component {
       return { modalOpen: false };
     });
   };
+  increment = id => {
+    console.log("increased");
+  };
+  decrement = id => {
+    console.log("decreased");
+  };
+  removeItem = id => {
+    console.log("item removed");
+  };
+  clearCart = () => {
+    console.log("cart cleared");
+  };
   render() {
     return (
       <ProductContext.Provider
@@ -74,7 +89,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
