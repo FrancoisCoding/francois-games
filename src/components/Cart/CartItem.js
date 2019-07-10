91b9ce3365da4670b69d98d66f34bfedf3dfe72d
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 15:30:47 -0400

    Changed decrement icon in cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -25,7 +25,7 @@ export default function CartItem({ item, value }) {
         <div className="d-flex justify-content-center">
           <div>
             <span className="btn btn-black mx-1" onClick={() => decrement(id)}>
-              ..
+              -
             </span>
             <span className="btn btn-black mx-1">{count}</span>
             <span className="btn btn-black mx-1" onClick={() => increment(id)}>
