cde7b35fb06f7061b31884ec4b9981d5c6339854
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 12:06:06 -0400

    Finished add to cart functionality

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import CartItem from "./CartItem";
-export default () => {
+export default function CartList({ value }) {
+  const { cart } = value;
+
   return (
-    <div>
-      hello from cart list
-      <CartItem />
+    <div className="container-fluid">
+      {cart.map(item => {
+        return <CartItem key={item.id} item={item} value={value} />;
+      })}
     </div>
   );
-};
+}
