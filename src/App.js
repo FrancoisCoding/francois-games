7dc76fff4cdecff4dfe25ac95e2cc5e9aab2d3bf
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 15:10:42 -0400

    Created footer component and added it to product page and details

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
 import Modal from "./components/Modal";
+
 function App() {
   return (
     <React.Fragment>
