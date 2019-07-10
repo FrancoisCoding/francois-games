45ca045e94738c7a9a83182fbbd8da2410f04f38
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-09 21:29:46 -0400

    Replaced placeholder text with updated info in details component

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const storeProducts = [
     price: 10,
     company: "GOOGLE",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The phone comes with a 5.00-inch touchscreen display with a resolution of 1080x1920 pixels at a pixel density of 441 pixels per inch (ppi). Google Pixel is powered by a 1.6GHz quad-core Qualcomm Snapdragon 821 processor. It comes with 4GB of RAM. ... Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage.",
     inCart: false,
     count: 0,
     total: 0
@@ -18,7 +18,7 @@ export const storeProducts = [
     price: 16,
     company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Samsung's brand new Galaxy S7 is a subtle improvement over its predecessor on the outside, but a whole new beast on the inside. Powered by the Exynos 8890 SoC, this phone can blaze through absolutely anything you throw at it, with power to spare. The camera is absolutely amazing, especially in low light. This is a pragmatic phone, compared to its sibling the Galaxy S7 Edge, and is better value for money if you don't want to show off. However, with competition at an all-time high, you can also get most of its features and functions for one third this price.",
     inCart: false,
     count: 0,
     total: 0
@@ -30,7 +30,7 @@ export const storeProducts = [
     price: 8,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The HTC 10 is a beautifully crafted smartphone with a gorgeous display and an ergonomic design. This phone is the first to have optical image stabilisation for the front facing camera, which does help when shooting in low light. The phone has very snappy performance thanks to the Snapdragon 820 SoC, 4GB of RAM and 32GB of onboard storage. The USB Type-C port gets USB 3.1 Gen 1 speeds for faster data transfers. Sense UI is now leaner than ever, with many of the custom apps traded in favour of Google's native apps. Camera performance is good thanks to the 12 Ultrapixel 2 sensor, OIS and laser autofocus. The built-in battery lasts an entire day but doesn't do too well if you watch a lot of videos. Overall, the HTC 10 is a solid flagship that can go toe-to-toe with the best in the business.",
     inCart: false,
     count: 0,
     total: 0
@@ -42,7 +42,7 @@ export const storeProducts = [
     price: 18,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The HTC 10 is a beautifully crafted smartphone with a gorgeous display and an ergonomic design. This phone is the first to have optical image stabilisation for the front facing camera, which does help when shooting in low light. The phone has very snappy performance thanks to the Snapdragon 820 SoC, 4GB of RAM and 32GB of onboard storage. The USB Type-C port gets USB 3.1 Gen 1 speeds for faster data transfers. Sense UI is now leaner than ever, with many of the custom apps traded in favour of Google's native apps. Camera performance is good thanks to the 12 Ultrapixel 2 sensor, OIS and laser autofocus. The built-in battery lasts an entire day but doesn't do too well if you watch a lot of videos. Overall, the HTC 10 is a solid flagship that can go toe-to-toe with the best in the business.",
     inCart: false,
     count: 0,
     total: 0
@@ -54,7 +54,7 @@ export const storeProducts = [
     price: 24,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "HTC Desire 626 is powered by a 1.2GHz quad-core Qualcomm Snapdragon 410 processor. It comes with 1GB of RAM. The HTC Desire 626 runs Android and is powered by a 2,000mAh non-removable battery. ... The HTC Desire 626 measures 146.90 x 70.90 x 8.19mm (height x width x thickness) and weighs 140.00 grams.",
     inCart: false,
     count: 0,
     total: 0
@@ -66,7 +66,7 @@ export const storeProducts = [
     price: 17,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Apple iPhone 5 is powered by a 1.3GHz dual-core Apple A6 processor. It comes with 1GB of RAM. The Apple iPhone 5 runs iOS 6.1.4 and is powered by a 1,440mAh non-removable battery. ... The Apple iPhone 5 measures 123.80 x 58.60 x 7.60mm (height x width x thickness) and weighs 112.00 grams.",
     inCart: false,
     count: 0,
     total: 0
@@ -78,7 +78,7 @@ export const storeProducts = [
     price: 30,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The iPhone 7 features a 28mm 12-megapixel camera with optical image stabilization, a wider f/1.8 aperture 6-element lens, wide color capture, and a revamped Apple image signal processor, all of which result in brighter, sharper, more detailed photos, even in low-light conditions.",
     inCart: false,
     count: 0,
     total: 0
