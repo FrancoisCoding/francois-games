08d504f2eeb00f644cddb5e4957dc50de419099c
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-10 00:32:38 -0400

    Started Modal.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -56,6 +56,7 @@ export default class extends Component {
                         disabled={inCart ? true : false}
                         onClick={() => {
                           value.addToCart(id);
+                          value.openModal(id);
                         }}
                       >
                         {inCart ? "inCart" : "add to cart"}
