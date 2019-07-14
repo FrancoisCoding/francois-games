72e0943b72c38639bf9d04f63eaf47e4a1fdf77f
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-14 18:02:45 -0400

    Enlarged Game Titles text

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ export default function Title({ name, title }) {
   return (
     <div className="row">
       <div className="col-10 mx-auto my-2 text-center text-title">
-        <h1 className="text-capitalize font-weight-bold">
+        <h1 className="text-capitalize font-weight-bold font-big">
           {name} <strong className="text-green">{title}</strong>
         </h1>
       </div>
